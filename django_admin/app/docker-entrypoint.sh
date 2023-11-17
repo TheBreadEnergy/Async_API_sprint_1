f7c8b845cf62caf117cb4d64be978ddce7920503
Samir Akhmed <warrior.owl@yandex.ru>
2023-11-17 12:13:58 +0300

    explicit python usage

--- django_admin/app/docker-entrypoint.sh
+++ django_admin/app/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-./manage.py collectstatic --noinput
+python ./manage.py collectstatic --noinput
 while ! nc -z "$DB_HOST" "$DB_PORT"; do sleep 1; done;
-./manage.py migrate
+python ./manage.py migrate
 uwsgi --strict --ini uwsgi.ini
